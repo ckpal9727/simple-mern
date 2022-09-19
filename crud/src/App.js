93f147d0c351bdbb86473a5057d7aa8f14ad6e55
ck <ckpal9727@gmail.com>
2022-09-19 21:25:07 +0530

    Set Home filr

--- crud/src/App.js
+++ crud/src/App.js
@@ -4,11 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div >
       <Routes>
+        <Route exact path='/' element={<Home/>}/>
         <Route exact path='/register' element={<Register/>}/>
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/profile' element={<Profile/>}/>
